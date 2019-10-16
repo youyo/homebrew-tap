d7276b2ba37b32c4425c64f13f810e64fc6da810
goreleaserbot <goreleaser@carlosbecker.com>
2019-10-17 00:47:49 +0900

    Brew formula update for awssh version v0.2.1

--- Formula/awssh.rb
+++ Formula/awssh.rb
@@ -2,16 +2,16 @@
 class Awssh < Formula
   desc "CLI tool to login ec2 instance"
   homepage "https://github.com/youyo/awssh"
-  version "0.1.3"
+  version "0.2.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/youyo/awssh/releases/download/0.1.3/awssh_0.1.3_Darwin_x86_64.tar.gz"
-    sha256 "c971e1bf605ad3fe28f78221d0e0b47735584bafff8bc83950d44a635a04b7cd"
+    url "https://github.com/youyo/awssh/releases/download/v0.2.1/awssh_0.2.1_Darwin_x86_64.tar.gz"
+    sha256 "008ba2a07707b9b136fbb2f70167603e06315d215dd408a7ade6c8ff4816b972"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/youyo/awssh/releases/download/0.1.3/awssh_0.1.3_Linux_x86_64.tar.gz"
-      sha256 "145aabe8f7e4e7bf5280b25d4307373345ce093a0b9ff526d93dfe90446161d8"
+      url "https://github.com/youyo/awssh/releases/download/v0.2.1/awssh_0.2.1_Linux_x86_64.tar.gz"
+      sha256 "9f81cb29f5a9853f0fbdb3f5af9f3fc209759c8899a6b46b8936493af564c200"
     end
   end
 
